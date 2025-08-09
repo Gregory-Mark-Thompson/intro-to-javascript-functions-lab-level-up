9067597b603fea28b5e93f44c698e1b7f90251e5
generalGreg <thompson.gregory.mark@gmail.com>
2025-08-09 00:10:01 -0700

    1st draft of G. Thompson's 1st hw level up.

---
+++ app.js
@@ -0,0 +1,39 @@
+// Exercise 10
+
+const calculateGrade = (gradeInput = 0) => {
+    if ((gradeInput/100) >= 90) {
+        return "A";
+    }     else if ((gradeInput/100) >= 80) {
+        return "B";
+    }     else if ((gradeInput/100) >= 70) {
+        return "C";
+    }     else if ((gradeInput/100) >= 60) {
+        return "D";
+    }     else {
+        return "F";
+    }
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85));
+
+
+// Exercise 11
+
+const createUsername = (firstNameInput, lastNameInput) => {
+    let lengthCalculated = firstNameInput.length + lastNameInput.length;
+    let slicedFirstName = firstNameInput.slice(0, 3);
+    let slicedLastName = lastNameInput.slice(0,3);
+    return (slicedFirstName + slicedLastName + lengthCalculated);
+}
+
+console.log('Exercise 11 Result:', createUsername('Samantha', 'Green'));
+
+
+// Exercise 12
+
+function numArgs(...userInput) {
+    return userInput.length;
+}
+
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
